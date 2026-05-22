140b4562617fb55f7d17f32db8a808f9b0da16d2
Alexandru <alexunt@umich.edu>
2026-05-22 07:34:32 -0400

    Not fully working risk version

--- Desktop/Github_Folder/CoinPredictor/DataScraper.cpp
+++ Desktop/Github_Folder/CoinPredictor/DataScraper.cpp
@@ -1,4 +1,8 @@
 //The objective of this is to gather previous data on select coins and 
 //determine using statistical trends what coins are likely to increase in the future
 
-cout "look at this";
+#include <iostream>
+
+void scrapeHistoricalCoinData() {
+    std::cout << "Data scraper is not implemented yet." << std::endl;
+}
